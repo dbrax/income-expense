9c805b105455f254e3001b8584eb157de797fce7
emmanuel <epmnzava@gmail.com>
2021-03-04 22:56:15 +0300

    added ledger

---
+++ src/Models/Ledger.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Author: Emmanuel Paul Mnzava
+ * Twitter: @epmnzava
+ * Github:https://github.com/dbrax/income-expense
+ * Email: epmnzava@gmail.com
+ * 
+ */
+
+namespace  Epmnzava\IncomeExpense\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ledger extends Model
+{
+
+    protected $guarded = [];
+    protected $table = "ledger";
+}
